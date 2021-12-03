1b53237be675a55c17a5d14f09a9d0434a353cc8
Anthony Mendez <738092+0x6d6e647a@users.noreply.github.com>
2021-12-02 22:45:03 -0800

    Add day 03 solutions.

---
+++ 03/life.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @o2;
+my @co2;
+
+while ( <STDIN> ) {
+    chomp;
+    push @o2, $_;
+    push @co2, $_;
+}
+
+my $bit_width = length $o2[0];
+
+for my $bit_index ( 0 .. $bit_width - 1 ) {
+    if ( scalar @o2 > 1 ) {
+        my $o2_num0  = 0;
+        my $o2_num1  = 0;
+
+        foreach my $o2_val ( @o2 ) {
+            my $val = substr($o2_val, $bit_index, 1);
+
+            if ( $val eq '0' ) {
+                $o2_num0 += 1;
+            } elsif ( $val eq '1' ) {
+                $o2_num1 += 1;
+            }
+        }
+
+        my $o2_target = undef;
+
+        if ( $o2_num0 > $o2_num1 ) {
+            $o2_target = 0;
+        } elsif ( $o2_num0 < $o2_num1 ) {
+            $o2_target = 1;
+        } else {
+            $o2_target = 1;
+        }
+
+        my @o2_new;
+
+        foreach my $o2_val (@o2) {
+            if ( substr($o2_val, $bit_index, 1) == $o2_target ) {
+                push @o2_new, $o2_val;
+            }
+        }
+
+        @o2 = @o2_new;
+    }
+
+    if ( scalar @co2 > 1 ) {
+        my $co2_num0 = 0;
+        my $co2_num1 = 0;
+
+        foreach my $co2_val ( @co2 ) {
+            my $val = substr($co2_val, $bit_index, 1);
+
+            if ( $val eq '0' ) {
+                $co2_num0 += 1;
+            } elsif ( $val eq '1' ) {
+                $co2_num1 += 1;
+            }
+        }
+
+        my $co2_target = undef;
+
+        if ( $co2_num0 > $co2_num1 ) {
+            $co2_target = 1;
+        } elsif ( $co2_num0 < $co2_num1 ) {
+            $co2_target = 0;
+        } else {
+            $co2_target = 0;
+        }
+
+        my @co2_new;
+
+        foreach my $co2_val (@co2) {
+            if ( substr($co2_val, $bit_index, 1) == $co2_target ) {
+                push @co2_new, $co2_val;
+            }
+        }
+
+        @co2 = @co2_new;
+    }
+
+    last if (scalar @o2 == 1 && scalar @co2 == 1);
+}
+
+my $o2_fin = eval '0b' . $o2[0];
+my $co2_fin = eval '0b' . $co2[0];
+
+print $o2_fin * $co2_fin . "\n";
---
+++ 03/rate.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @bit_fields;
+
+while ( <STDIN> ) {
+    chomp;
+    for my $i ( 0 .. length($_) - 1 ) {
+        my $bit = substr($_, $i, 1);
+        $bit_fields[$i]{$bit} += 1;
+    }
+}
+
+my $gamma = '';
+my $epsilon = '';
+
+foreach (@bit_fields) {
+    my $num0 = $_->{'0'};
+    my $num1 = $_->{'1'};
+
+    if ( $num0 > $num1 ) {
+        $gamma   .= '0';
+        $epsilon .= '1';
+    } else {
+        $gamma   .= '1';
+        $epsilon .= '0';
+    }
+}
+
+$gamma = eval '0b' . $gamma;
+$epsilon = eval '0b' . $epsilon;
+
+print $gamma * $epsilon . "\n";
